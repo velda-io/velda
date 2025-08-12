e4daa1625582bafa742d80109ed003a207716300
eagleonhill <eagleonhill@users.noreply.github.com>
2025-08-12 12:04:08 -0700

    Use instance-id as aws agent name. This can make agent start faster. (#25)
    Skip the agent to query for instance name on boot.
    
    Before: 30s
    
    After: 12s from boot
    
    Tested with other systemd change in separate change.

--- pkg/broker/backends/aws/aws.go
+++ pkg/broker/backends/aws/aws.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -29,29 +29,30 @@ import (
 )
 
 type awsPoolBackend struct {
-	region             string
-	launchTemplateId   string
-	instanceNamePrefix string
-	svc                *ec2.EC2
-	lastOp             *ec2.DescribeInstancesOutput
-	instanceIdMap      map[string]string
+	region              string
+	launchTemplateId    string
+	instanceNamePrefix  string
+	useInstanceIdAsName bool
+	svc                 *ec2.EC2
+	lastOp              *ec2.DescribeInstancesOutput
+	instanceIdMap       map[string]string
 }
 
-func NewAWSPoolBackend(region, launchTemplateId, instanceNamePrefix string,
+func NewAWSPoolBackend(region, launchTemplateId, instanceNamePrefix string, useInstanceIdAsName bool,
 	svc *ec2.EC2) broker.ResourcePoolBackend {
 	// Fetch template ID from name
 	return &awsPoolBackend{
-		region:             region,
-		launchTemplateId:   launchTemplateId,
-		instanceNamePrefix: instanceNamePrefix,
-		svc:                svc,
-		instanceIdMap:      make(map[string]string),
+		region:              region,
+		launchTemplateId:    launchTemplateId,
+		instanceNamePrefix:  instanceNamePrefix,
+		useInstanceIdAsName: useInstanceIdAsName,
+		svc:                 svc,
+		instanceIdMap:       make(map[string]string),
 	}
 }
 
 func (a *awsPoolBackend) RequestScaleUp(ctx context.Context) (string, error) {
 	nameSuffix := utils.RandString(5)
-	name := fmt.Sprintf("%s%s", a.instanceNamePrefix, nameSuffix)
 	input := &ec2.RunInstancesInput{
 		LaunchTemplate: &ec2.LaunchTemplateSpecification{
 			LaunchTemplateId: aws.String(a.launchTemplateId),
@@ -59,7 +60,11 @@ func (a *awsPoolBackend) RequestScaleUp(ctx context.Context) (string, error) {
 		},
 		MinCount: aws.Int64(1),
 		MaxCount: aws.Int64(1),
-		TagSpecifications: []*ec2.TagSpecification{
+	}
+	var name string
+	if !a.useInstanceIdAsName {
+		name = fmt.Sprintf("%s%s", a.instanceNamePrefix, nameSuffix)
+		input.TagSpecifications = []*ec2.TagSpecification{
 			{
 				ResourceType: aws.String("instance"),
 				Tags: []*ec2.Tag{
@@ -69,38 +74,49 @@ func (a *awsPoolBackend) RequestScaleUp(ctx context.Context) (string, error) {
 					},
 				},
 			},
-		},
+		}
 	}
 	result, err := a.svc.RunInstancesWithContext(ctx, input)
 	if err != nil {
 		return "", err
 	}
-	log.Printf("Created instance %s", *result.Instances[0].InstanceId)
-	a.instanceIdMap[name] = *result.Instances[0].InstanceId
+	instanceName := *result.Instances[0].InstanceId
+	if a.useInstanceIdAsName {
+		name = instanceName
+	} else {
+		a.instanceIdMap[name] = instanceName
+	}
+	log.Printf("Created instance %s AS %s", *result.Instances[0].InstanceId, name)
 	//a.lastOp = result
 	return name, nil
 }
 
 func (a *awsPoolBackend) RequestDelete(ctx context.Context, workerName string) error {
-	instanceId, ok := a.instanceIdMap[workerName]
-	if !ok {
-		// Query from EC2
-		input := &ec2.DescribeInstancesInput{
-			Filters: []*ec2.Filter{
-				{
-					Name:   aws.String("tag:Name"),
-					Values: []*string{aws.String(workerName)},
+	var instanceId string
+	if a.useInstanceIdAsName {
+		instanceId = workerName
+	} else {
+		var ok bool
+		instanceId, ok = a.instanceIdMap[workerName]
+		if !ok {
+			// Query from EC2
+			input := &ec2.DescribeInstancesInput{
+				Filters: []*ec2.Filter{
+					{
+						Name:   aws.String("tag:Name"),
+						Values: []*string{aws.String(workerName)},
+					},
 				},
-			},
+			}
+			result, err := a.svc.DescribeInstancesWithContext(ctx, input)
+			if err != nil {
+				return fmt.Errorf("Failed to find instance %s: %v", workerName, err)
+			}
+			if len(result.Reservations) == 0 || len(result.Reservations[0].Instances) == 0 {
+				return fmt.Errorf("Failed to find instance %s", workerName)
+			}
+			instanceId = *result.Reservations[0].Instances[0].InstanceId
 		}
-		result, err := a.svc.DescribeInstancesWithContext(ctx, input)
-		if err != nil {
-			return fmt.Errorf("Failed to find instance %s: %v", workerName, err)
-		}
-		if len(result.Reservations) == 0 || len(result.Reservations[0].Instances) == 0 {
-			return fmt.Errorf("Failed to find instance %s", workerName)
-		}
-		instanceId = *result.Reservations[0].Instances[0].InstanceId
 	}
 	input := &ec2.TerminateInstancesInput{
 		InstanceIds: []*string{aws.String(instanceId)},
@@ -133,14 +149,18 @@ func (a *awsPoolBackend) ListWorkers(ctx context.Context) ([]broker.WorkerStatus
 				continue
 			}
 			name := ""
-			for _, tag := range instance.Tags {
-				if *tag.Key == "Name" {
-					name = *tag.Value
-					break
+			if !a.useInstanceIdAsName {
+				for _, tag := range instance.Tags {
+					if *tag.Key == "Name" {
+						name = *tag.Value
+						break
+					}
 				}
-			}
-			if name == "" {
-				log.Printf("Instance %s has no Name tag", *instance.InstanceId)
+				if name == "" {
+					log.Printf("Instance %s has no Name tag", *instance.InstanceId)
+					name = *instance.InstanceId
+				}
+			} else {
 				name = *instance.InstanceId
 			}
 			res = append(res, broker.WorkerStatus{
@@ -192,7 +212,7 @@ func (f *awsPoolFactory) NewBackend(pb *proto.AutoscalerBackend) (broker.Resourc
 		return nil, fmt.Errorf("Launch template %s not found", awsTemplate.LaunchTemplateName)
 	}
 	launchTemplateId := *result.LaunchTemplates[0].LaunchTemplateId
-	return NewAWSPoolBackend(awsTemplate.Region, launchTemplateId, prefix, svc), nil
+	return NewAWSPoolBackend(awsTemplate.Region, launchTemplateId, prefix, awsTemplate.UseInstanceIdAsName, svc), nil
 }
 
 func init() {
