8423e86823b5de35226779ba9fd76752e81457b4
eagleonhill <eagleonhill@users.noreply.github.com>
2025-08-20 22:55:33 -0700

    Add pool command (#72)

---
+++ client/cmd/pool_list.go
@@ -0,0 +1,49 @@
+// Copyright 2025 Velda Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/spf13/cobra"
+
+	"velda.io/velda/pkg/clientlib"
+	"velda.io/velda/pkg/proto"
+)
+
+var listPoolCmd = &cobra.Command{
+	Use:   "list",
+	Short: "List all available pools",
+	Args:  cobra.NoArgs,
+	RunE: func(cmd *cobra.Command, args []string) error {
+		conn, err := clientlib.GetApiConnection()
+		if err != nil {
+			return fmt.Errorf("Error getting API connection: %w", err)
+		}
+		client := proto.NewPoolManagerServiceClient(conn)
+
+		pools, err := client.ListPools(cmd.Context(), &proto.ListPoolsRequest{})
+		if err != nil {
+			return fmt.Errorf("Error listing pools: %w", err)
+		}
+		for _, pool := range pools.Pools {
+			fmt.Printf("%s\n", pool.Name)
+		}
+		return nil
+	},
+}
+
+func init() {
+	poolCmd.AddCommand(listPoolCmd)
+}
