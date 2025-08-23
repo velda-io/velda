f6fd109cf25cf533dc36543918d46f8db4387eca
Chuan Qiu <chuanq@velda.io>
2025-08-22 22:51:59 -0700

    Add some logging for the status

--- pkg/clientlib/session.go
+++ pkg/clientlib/session.go
@@ -4,7 +4,7 @@
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//     http://www.apache.org/licenses/LICENSE-2.0
+//	http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -27,8 +27,8 @@ import (
 	"golang.org/x/crypto/ssh"
 	pb "google.golang.org/protobuf/proto"
 
-	"velda.io/velda/pkg/term"
 	"velda.io/velda/pkg/proto"
+	"velda.io/velda/pkg/term"
 )
 
 type SshSession struct {
@@ -110,7 +110,9 @@ func (s *SshSession) RequestPty(force bool) error {
 
 func (s *SshSession) RestoreTty() error {
 	if s.oldState != nil {
-		return term.Restore(int(s.ttyFd), s.oldState)
+		oldState := s.oldState
+		s.oldState = nil
+		return term.Restore(int(s.ttyFd), oldState)
 	}
 	return nil
 }
