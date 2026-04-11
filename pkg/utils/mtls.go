7feeea214c0f16024f213debf19e2a967e138d6b
eagleonhill <eagleonhill@users.noreply.github.com>
2026-04-10 20:57:27 -0700

    add headers (#264)

--- pkg/utils/mtls.go
+++ pkg/utils/mtls.go
@@ -1,3 +1,16 @@
+// Copyright 2025 Velda Inc
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//	http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 package utils
 
 import (
@@ -64,7 +77,7 @@ func BuildMTLSClientTransportCredentials(clientCertPath, clientKeyPath, serverRo
 	}
 	bundle, err := x509bundle.Parse(expectedServerID.TrustDomain(), caPem)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse server root CA at %s", serverRootCASource)
+		return nil, fmt.Errorf("failed to parse server root CA at %s: %w", serverRootCASource, err)
 	}
 
 	return credentials.NewTLS(tlsconfig.MTLSClientConfig(
@@ -101,7 +114,7 @@ func BuildMTLSServerTLSConfig(tlsCertPath, tlsKeyPath, clientRootCASource, requi
 	}
 	bundle, err := x509bundle.Parse(expectedClientID.TrustDomain(), caPem)
 	if err != nil {
-		return nil, fmt.Errorf("failed to parse client root CA at %s", clientRootCASource)
+		return nil, fmt.Errorf("failed to parse client root CA at %s: %w", clientRootCASource, err)
 	}
 
 	return tlsconfig.MTLSServerConfig(
